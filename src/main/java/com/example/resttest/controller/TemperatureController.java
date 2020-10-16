da33a55e8bafd6bfff18fe23ec77c3819ce70072
Rasmus Lynge Jacobsen <38203575+RasmusLynge@users.noreply.github.com>
2020-10-16 14:24:08 +0200

    Update TemperatureController.java

--- src/main/java/com/example/resttest/controller/TemperatureController.java
+++ src/main/java/com/example/resttest/controller/TemperatureController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class TemperatureController {
 
